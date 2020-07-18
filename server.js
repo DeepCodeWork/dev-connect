2ec31a33f489d09429094075d2b0099df59a14b2
makingout456 <kumardeep245@gmail.com>
2020-07-19 00:59:04 +0530

    introducing front-end

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen( PORT, ()=>{
     console.log(`App running on ${PORT}`)
